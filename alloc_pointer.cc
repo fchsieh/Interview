fa808f8d8ede45020ad895f99e5601b804447997
Fred Hsieh <fhsieh@uci.edu>
2022-11-27 17:51:03 -0800

    Init

---
+++ alloc_pointer.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int **cpp_style_alloc(int n) {
+    int **a = new int *[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = new int[n];
+    }
+    return a;
+}
+
+int **c_style_alloc(int n) {
+    int **a = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        a[i] = (int *)malloc(n * sizeof(int));
+    }
+    return a;
+}
+
+void cpp_style_free(int **a, int n) {
+    for (int i = 0; i < n; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+}
+
+void c_style_free(int **a, int n) {
+    for (int i = 0; i < n; i++) {
+        free(a[i]);
+    }
+    free(a);
+}
+
+int main() {
+    int **cpp = cpp_style_alloc(10);
+    int **c = c_style_alloc(10);
+
+    cpp_style_free(cpp, 10);
+    c_style_free(c, 10);
+
+    return 0;
+}
---
+++ hashtable.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int myhash(string key, int size) { return key.length() % size; }
+int myhash(int key, int size) { return key % size; }
+
+template <typename K, typename V>
+class HashMap {
+   private:
+    int TABLE_SIZE;
+    vector<vector<pair<K, V> > > table;
+
+   public:
+    HashMap(int size) {
+        TABLE_SIZE = size;
+        for (int i = 0; i < size; i++) table.push_back({});
+    }
+    ~HashMap() { delete[] table; }
+    void insert(K key, V value) {
+        int hashValue = myhash(key, TABLE_SIZE);
+        // check if key is already present
+        if (get(key) != (V)-1) {
+            // update the value of the key
+            for (int i = 0; i < table[hashValue].size(); i++) {
+                if (table[hashValue][i].first == key) {
+                    table[hashValue][i] = {key, value};
+                    return;
+                }
+            }
+        } else {
+            table[hashValue].push_back({key, value});
+        }
+    }
+    V get(K key) {
+        int hashValue = myhash(key, TABLE_SIZE);
+        for (const auto [k, v] : table[hashValue]) {
+            if (k == key) return v;
+        }
+        return (V)-1;
+    }
+    bool remove(K key) {
+        int hashValue = myhash(key, TABLE_SIZE);
+        if (get(key) != (V)-1) {
+            for (int i = 0; i < table[hashValue].size(); i++) {
+                if (table[hashValue][i].first == key) {
+                    // found!
+                    table[hashValue].erase(table[hashValue].begin() + i);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    HashMap<string, int> *table = new HashMap<string, int>(10);
+    table->insert("hello", 1);
+    cout << table->get("hello") << endl;
+    table->insert("world", 2);
+    table->insert("hello", 3);
+    cout << table->get("world") << endl;
+    cout << table->get("hello") << endl;
+    table->remove("hello");
+    cout << table->get("hello") << endl;
+
+    HashMap<int, int> *table2 = new HashMap<int, int>(10);
+    table2->insert(1, 123);
+    table2->insert(2, 456);
+    cout << table2->get(1) << endl;
+    cout << table2->get(3) << endl;
+    table2->insert(1, 222);
+    cout << table2->get(1) << endl;
+
+    return 0;
+}
